6e16b5c4b2bd3d882cdee389979b9e1f47bff158
Nethmi Kaveesha <nethmikaveesha593@gmail.com>
2026-02-09 10:56:39 +0530

    Updated ViewReservation.jsx

--- frontend/src/Pages/ViewReservation.jsx
+++ frontend/src/Pages/ViewReservation.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../services/api";
+import api from "../Services/api";
 
 const ViewReservation = () => {
   const [reservations, setReservations] = useState([]);
